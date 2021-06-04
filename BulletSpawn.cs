e16006a3792192835b3272844ddb9438c993175d
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-04 09:14:42 +0900

    Add files via upload

---
+++ BulletSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    
+
+
+    GameObject clone;
+    Rigidbody clone_rb;
+    public GameObject bullet;
+    void Start()
+    {
+
+        clone_rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {       
+        
+
+        if(Input.GetMouseButton(0))
+        {            
+            clone = Instantiate(bullet, transform.position, Quaternion.Euler(90f+transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0));
+            clone_rb = clone.GetComponent<Rigidbody>();
+            clone_rb.velocity(transform.forward * 1600f);
+        }
+    }
+}
---
+++ move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Transform head_tr;
+
+    float MouseX;
+    float MouseY;
+    public float sens = 3f;
+    float v;
+    float h;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        head_tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        MouseX = MouseX + Input.GetAxis("Mouse X") * sens;
+        MouseY = MouseY - Input.GetAxis("Mouse Y") * sens;
+
+        MouseY = Mathf.Clamp(MouseY, -40, 40);
+
+        head_tr.rotation = Quaternion.Euler(MouseY, MouseX, 0);
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");   
+        rb.velocity = transform.forward * v * 5f;
+    }
+}
